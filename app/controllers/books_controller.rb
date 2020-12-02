39931b64b732c1b8b271a157e6abae2a672f5a61
takako-K <takako.k828@gmail.com>
2020-12-02 13:44:13 +0000

    [Add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,48 @@
 class BooksController < ApplicationController
   def show
+    @book = Book.find(params[:id])
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def create
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
+    redirect_to book_path(@book.id), notice: "Book was successfully created."
+  else
+    render action: :index
+  end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    redirect_to book_path(@book.id), notice: "Book was successfully updated."
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path, notice: "Book was successfully destroyed."
+  end
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :opinion)
+  end
+
+
 end
+
