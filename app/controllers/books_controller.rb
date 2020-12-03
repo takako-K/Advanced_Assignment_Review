8e691da2f40594b4fed7f088ffe8529a78b76f9e
takako-K <takako.k828@gmail.com>
2020-12-03 04:59:27 +0000

    [Fix]error修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,6 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @books = Book.all
     if @book.save
     redirect_to book_path(@book.id), notice: "Book was successfully created."
   else
@@ -40,7 +39,7 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.require(:book).permit(:title, :opinion)
+    params.require(:book).permit(:title, :body)
   end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @book = Book.new
   end
 
   def index
+    @users = User.all
   end
 
   def edit
