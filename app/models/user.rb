2a384546fc0e9d3bbb27abdbaec48cad163de8eb
takako-K <takako.k828@gmail.com>
2020-12-04 11:42:16 +0000

    [Add]

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :books, dependent: :destroy
+
   attachment :profile_image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'homes#top'
+
   resources :users
   resources :books
+
 end
