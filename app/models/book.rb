65ab1a10a95affe336fd8600a03c4974488d3578
takako-K <takako.k828@gmail.com>
2020-12-23 07:09:33 +0000

    [Add]いいね機能　ルート・モデル・コントローラーの作成

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,10 @@ class Book < ApplicationRecord
   validates :body, presence: true, length: { maximum: 200 }
 
   belongs_to :user, optional: true
+  # いいね機能
+  has_many :favorites, dependent: :destroy
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
 
   resources :users
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
 
 end
