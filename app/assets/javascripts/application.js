fcd48dc7f6c7015e0de056a2224f64c2a2af4739
takako-K <takako.k828@gmail.com>
2020-12-23 05:44:13 +0000

    [Ready]Advanced_Assignment_Review

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,12 +18,3 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-
-var title ="javascriptが使えました";
-alert(title);
-
-$(document).ready(function () {
-  $('.jquery').on('click', function(){
-    $(this).css('color','red');
-  });
-});
