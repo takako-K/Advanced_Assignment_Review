559f8288d7087d7f03d7baed33b590f4af77637a
takako-K <takako.k828@gmail.com>
2020-12-23 11:54:34 +0000

    [Add]コントローラー・ルーティング設定

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,5 +8,7 @@ class Book < ApplicationRecord
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
+  # コメント機能
+  has_many :book_comments, dependent: :destroy
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   # いいね機能
   has_many :favorites, dependent: :destroy
+  # コメント機能
+  has_many :book_comments, dependent: :destroy
 
   attachment :profile_image
 
