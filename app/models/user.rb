c05b748d88d73e2301f0bc49a5ae0a8cd2d70992
takako-K <takako.k828@gmail.com>
2020-12-24 06:32:01 +0000

    [Start]フォロー・フォロワー機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,29 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
+
   # いいね機能
   has_many :favorites, dependent: :destroy
+
   # コメント機能
   has_many :book_comments, dependent: :destroy
+
+  #フォロー・フォロワー機能
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followed_user, through: :follower, source: :followed
+  has_many :follower_user, through: :followed, source: :follower
+
+  def create(user_id)
+    follower.create(followed_id: user_id)
+  end
+  def destroy(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+  def following?(user)
+    following_user.include?(user)
+  end
+
 
   attachment :profile_image
 
