1f795aaf54dde4951dcba42e959cd62a1f1ef5b5
takako-K <takako.k828@gmail.com>
2021-01-01 13:21:17 +0000

    [Fix]favoriteコントローラー修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,14 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
     #非同期化の為削除　 redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
     #非同期化の為削除　 redirect_back(fallback_location: root_path)
   end
